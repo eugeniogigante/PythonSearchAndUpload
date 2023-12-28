19e951e14721a8baaf3f6389e84bc665d81a07ff
Eugenio <eugenio.gigante@gmail.com>
2023-12-28 13:26:33 +0000

    Update upload.php

--- writefile.php
+++ writefile.php
@@ -1,27 +1,11 @@
-<--http://192.168.1.98/writefile.php?text_=test
--->
-
 <?php
-      if( $_GET["text_"]) {
-      $textreceived = $_GET['text_'];
-      echo "Welcome ". $textreceived . "<br />";
-	  
-	  $file = 'myfile.txt';
-		// The new person to add to the file
-		// Write the contents to the file, 
-		// using the FILE_APPEND flag to append the content to the end of the file
-		// and the LOCK_EX flag to prevent anyone else writing to the file at the same time
-		file_put_contents($file, $textreceived , FILE_APPEND | LOCK_EX);
-      
-      exit();
-   }
+if ($_FILES['file']['error'] === UPLOAD_ERR_OK) {
+    $tempFile = $_FILES['file']['tmp_name'];
+    $targetPath = 'uploads/';
+    $targetFile = $targetPath . basename($_FILES['file']['name']);
+    move_uploaded_file($tempFile, $targetFile);
+    echo "File uploaded successfully!";
+} else {
+    echo "File upload failed.";
+}
 ?>
-<html>
-   <body>  
-	  <form action = "<?php $_PHP_SELF ?>" method = "GET">
-         Name: <input type = "text" name = "text_" />
-         <input type = "submit" />
-      </form>
-   
-   </body>
-</html>
